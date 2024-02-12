75c14c1e6e716bd97c23659cd94bbe9733aec77b
Kavishka Indeepa <101946925+KavishkaIndeepa@users.noreply.github.com>
2024-02-13 00:31:24 +0530

    create project

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className={"text-red-600 font-bold font-serif items-center"}>Hello Travels</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/view/pages/Places/Places.tsx
@@ -0,0 +1,9 @@
+import {Component} from "react";
+
+export class Places extends Component {
+    render() {
+        return (
+            <></>
+        );
+    }
+}
