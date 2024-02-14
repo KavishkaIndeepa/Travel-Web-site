4bf4c86e75d7729d69de3d87b83f08354db9770e
Kavishka Indeepa <101946925+KavishkaIndeepa@users.noreply.github.com>
2024-02-14 16:03:30 +0530

    update home

--- client/src/view/common/Footer/Footer.tsx
+++ client/src/view/common/Footer/Footer.tsx
@@ -3,7 +3,12 @@ import {Component} from "react";
 export class Footer extends Component {
     render() {
         return (
-            <></>
+            <div className="p-2 bg-[#2B7A0B] flex justify-center">
+
+                <p className="text[10px] text-[#e6f0e6] pr-2 hover:text-green-400">Copyright @2023</p>
+                <h1 className="text-1xl text-third hover:text-white">Kavishka Indeepa</h1>
+
+            </div>
         );
     }
 }
--- client/src/view/pages/Home/Home.tsx
+++ client/src/view/pages/Home/Home.tsx
@@ -3,9 +3,23 @@ import {Component} from "react";
 export class Home extends Component {
     render() {
         return (
-            <>
-                <h1>This is Home</h1>
-            </>
+            <div className={"flex-row flex-wrap"}>
+
+                <div className={"flex min-h-screen bg-cover"} style={{backgroundImage: 'url("/Nine-Arch-Bridge.jpg")'}}></div>
+
+                <div className={"flex h-auto p-10 bg-black mt-5 mb-5 rounded-3xl"}>
+                    <div className={"flex-row text-white justify-center"}>
+                        “Visit our website and find the best and        most comfortable place for your journey”</div>
+                </div>
+
+                <div className={"flex-wrap"}>
+                    <div className={"flex-col w-2/12 h-auto justify-items-start bg-black"}></div>
+                    <div className={"flex-col justify-items-end bg-blue-600"}></div>
+                    <div className={"flex-col justify-items-start bg-gray-600"}></div>
+                    <div className={"flex-col justify-items-end bg-red-600"}></div>
+                </div>
+
+            </div>
         );
     }
 }
